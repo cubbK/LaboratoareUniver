ff4eadd0f8c431374bae1698e6f3d04dfa4be966
Dan <danpopa1998@gmail.com>
2016-12-08 01:52:27 +0200

    gata de aratat

---
+++ Web/dec2016SiteIarna/faraBootstrap/js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items : 1,
+    itemsDesktop : false,
+    singleItem : true
+  });
+});
--- Web/dec2016SiteIarna/js/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    items : 1,
-    itemsDesktop : false,
-    singleItem : true
-  });
-});

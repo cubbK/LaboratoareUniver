46d68e8827a460b5c20d16498cfa4d8992cbd438
Dan <danpopa1998@gmail.com>
2017-02-25 18:17:38 +0200

    facut 2 conditii
    treb sa adaug un gitignore mai bun

--- CPlusPlus/Laborator1/Laborator1/Laborator1.cpp
+++ CPlusPlus/Laborator1/Laborator1/Laborator1.cpp
@@ -4,31 +4,48 @@
 #include "stdafx.h"
 #include <iostream>
 #include <vector>
+#include <stdlib.h>  
 using namespace std;
 
 int minValue(vector<int> vals) {
 	int minVal = vals[0];
-	for (int i = 1; i < vals.size(); i++) {
-		if (minVal > vals[i]) {
-			minVal = vals[i];
+	for (int i : vals) {
+		if (minVal > i) {
+			minVal = i;
 		}
 	}
 	return minVal;
 }
 int maxValue(vector<int> vals) {
 	int maxVal = vals[0];
-	for (int i = 1; i < vals.size(); i++) {
-		if (maxVal < vals[i]) {
-			maxVal = vals[i];
+	for (int i : vals) {
+		if (maxVal < i) {
+			maxVal = i;
 		}
 	}
+	
 	return maxVal;
 }
-int main()
-{
-	vector<int> values = { 100,200,500,43,87,75 };
-	cout << minValue(values) << " "  << maxValue(values) <<  endl;
+int difference(int num1, int num2) {
+	return abs(num1 - num2);
+}
+float average(vector<int> vals) {
+	int total = 0;
+	for (int i : vals) {
+		total += i;
+	}
+	return (float)total / vals.size();
+}
 
+int main()
+{	
+	//numarul total de valori 
+	
+	vector<int> values = { 1,100,500,7658,5 };
+	cout << "Valoarea minima e " << minValue(values) << endl;
+	cout << "Valoarea maxima e " << maxValue(values) << endl;
+	cout << "Decalajul dintre valoarea maxima si minima e " << difference(minValue(values),maxValue(values)) << endl;
+	cout << "Valoarea medie a numerelor e " << average(values) << endl;
     return 0;
 }
 

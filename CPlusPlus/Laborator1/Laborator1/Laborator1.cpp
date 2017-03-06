5dd2fab317d2753799061741200e9384ec62ace4
Dan <danpopa1998@gmail.com>
2017-03-06 21:18:16 +0200

    terminat lab1

--- CPlusPlus/Laborator1/Laborator1/Laborator1.cpp
+++ CPlusPlus/Laborator1/Laborator1/Laborator1.cpp
@@ -1,7 +1,7 @@
 // Laborator1.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#include "stdafx.h" //vs headers
 #include <iostream>
 #include <vector>
 #include <map>
@@ -55,18 +55,70 @@ map <char, int> categorize(vector<int> vals, int controlPoint) {
 	}
 	return categories;
 }
-int main()
-{	
-	//numarul total de valori 
-	
-	vector<int> values = { 1,100,500,7658,5 };
-	cout << "Valoarea minima e " << minValue(values) << endl;
-	cout << "Valoarea maxima e " << maxValue(values) << endl;
-	cout << "Decalajul dintre valoarea maxima si minima e " << difference(minValue(values),maxValue(values)) << endl;
-	cout << "Valoarea medie a numerelor e " << average(values) << endl;
-	int controlPoint = 5;
+void showCategories(vector<int> values) {
+	int controlPoint;
+	cout << "introduceti punctul de control" << endl;
+	cin >> controlPoint;
 	map<char, int> categories = categorize(values, controlPoint);
 	cout << "Sunt " << categories['<'] << " numere mai mici ca " << controlPoint << ", " << categories['>'] << " mai mari si " << categories['='] << " egale" << endl;
+}
+int option() {
+	cout << "alegeti optiunea dorita" << endl;
+	cout << "(0) Afisarea celei mai mici valori" << endl;
+	cout << "(1) Afisarea celei mai mari valori" << endl;
+	cout << "(2) Afisarea diferentei dintre celei mai mari si celei mai mici valori" << endl;
+	cout << "(3) Afisarea mediei valorilor" << endl;
+	cout << "(4) Categorizarea numerelor" << endl;
+	int option;
+	cin >> option;
+	return option;
+}
+void execute(int option, vector<int> values) {
+	switch (option){
+	case 0 :
+		cout << "Valoarea minima e " << minValue(values) << endl;
+		break;
+	case 1 :
+		cout << "Valoarea maxima e " << maxValue(values) << endl;
+		break;
+	case 2 : 
+		cout << "Decalajul dintre valoarea maxima si minima e " << difference(minValue(values), maxValue(values)) << endl;
+		break;
+	case 3 :
+		cout << "Valoarea medie a numerelor e " << average(values) << endl;
+		break;
+	case 4 :
+		showCategories(values);
+		break;
+	default:
+		cout << "optiune gresita" << endl;
+		break;
+	}
+	
+}
+int main()
+{	
+	// introducerea datelor initiale
+	vector<int> values;
+	int nrVals;
+	cout << "Introduceti nr de valori" << endl;
+	cin >> nrVals;
+	cout << "Introduceti valorile" << endl;
+	for (int i = 0; i < nrVals; i++) {
+		int val;
+		cin >> val;
+		values.push_back(val);
+	}
+
+	execute(option(),values);
+	
+	
+	
+	
+	
+	
+	
+	
     return 0;
 }
 

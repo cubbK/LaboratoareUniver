720d53765f4bc6de7cfeb19b312272b93108b405
cubbic <danpopa1998@gmail.com>
2017-05-11 11:10:26 +0300

    add laborator6 conditii

--- CPlusPlus/Laborator6/main.cpp
+++ CPlusPlus/Laborator6/main.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+
+//Laborator 6
+int main() {
+    cout << "(0) De inscris datele initiale intr-un fisier binar dintr-un fisier text" << endl;
+    cout << "(1) De inscris datele initiale intr-un fisier binar de la tastatura" << endl;
+    cout << "(2) De adaugat date in fisier." << endl;
+    cout << "(3) De afisat continutul fisierului initial / intermediar / final." << endl;
+    cout << "(4) De aranjat datele din fisier dupa id" << endl;
+    cout << "(5) De aranjat datele din fisier dupa id si nume" << endl;
+    cout << "(6) De sters datele din fisier cu cel mai mare id" << endl;
+    cout << "(7) De calculat numarul datelor inscrise" << endl;
+    cout << "(8) De afisat 3 liste tematice" << endl;
+
+    int  choise;
+    cout << "Alegetei varianta" << endl;
+    cin >> choise;
+
     return 0;
 }
